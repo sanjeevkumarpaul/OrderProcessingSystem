96ff3d2b814de4b039bafc9db1d965f218cfeef8
Sanjeev Paul <sanjeevkumarpaul@hotmail.com>
2025-08-27 17:12:09 -0400

    delete and refactor unused ones

--- OrderProcessingSystem.API/Mapping/ApiMappingProfile.cs
+++ OrderProcessingSystem.API/Mapping/ApiMappingProfile.cs
@@ -1,7 +1,6 @@
 using AutoMapper;
 using OrderProcessingSystem.Data.Entities;
-using OrderProcessingSystem.Core.Entities;
-//using OrderProcessingSystem.Infrastructure.Dto;
+
 using OrderProcessingSystem.Contracts.Dto;
 using OrderProcessingSystem.Data.MediatorCQRS.Customers;
 using OrderProcessingSystem.Data.MediatorCQRS.Suppliers;
@@ -14,10 +13,8 @@ public class ApiMappingProfile : Profile
     {
         CreateMap<OrderProcessingSystem.Data.Entities.Supplier, SupplierDto>();
         CreateMap<OrderProcessingSystem.Data.Entities.Customer, CustomerDto>();
-        CreateMap<OrderProcessingSystem.Data.Entities.Order, OrderDto>();
-        CreateMap<OrderProcessingSystem.Core.Entities.Order, OrderDto>();
+        CreateMap<OrderProcessingSystem.Data.Entities.Order, OrderDto>();        
         CreateMap<OrderProcessingSystem.Data.MediatorCQRS.Reports.SalesByCustomerDto, SalesByCustomerDto>();
-        //CreateMap<OrderProcessingSystem.Infrastructure.Dto.SalesByCustomerReportDto, SalesByCustomerDto>();
         
         // Add mappings between Data layer DTOs and Contract layer DTOs
         CreateMap<OrderProcessingSystem.Data.MediatorCQRS.Customers.CustomerWithOrdersDto, OrderProcessingSystem.Contracts.Dto.CustomerWithOrdersDto>();
